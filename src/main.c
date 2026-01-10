59f692409e54f2a4ae7e3a638d340d671660ee0d
Ragg967 <rywatson1027@gmail.com>
2026-01-10 11:25:26 -0800

    getting there

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#define INT_TO_PTR(a) ((void *)(intptr_t)a)
 
 struct Node {
   void *data;
@@ -12,50 +15,34 @@ struct LinkedList {
   struct Node *list[];
 };
 
-struct LinkedList InitLinkedList(int nodeCount);
-void *listGetData(struct LinkedList list, int index);
-void listAppend(struct LinkedList list, void *data);
+struct LinkedList *InitLinkedList(int nodeCount);
+void *listGetData(struct LinkedList *list, size_t index);
+void listAppend(struct LinkedList *list, size_t index, void *data);
 
 int main(void) {
-  struct LinkedList list = InitLinkedList(10);
-  
+  struct LinkedList *list = InitLinkedList(10);
+  listAppend(list, 0, INT_TO_PTR(10));
   printf("%d", (int)(size_t)listGetData(list, 0));
 }
 
-void listAppend(struct LinkedList list, void *data) {
+void listAppend(struct LinkedList *list, size_t index, void *data) {
   struct Node *newNode = malloc(sizeof(struct Node));
   newNode->data = data;
-  list.list[list.nodeCount + 1] = newNode;
+  list->list[index] = newNode;
 }
 
-void *listGetData(struct LinkedList list, int index) {
+void *listGetData(struct LinkedList *list, size_t index) {
   for (size_t i = 0; i < index; i++) {
     if (i == index)
-      return list.list[i];
+      return list->list[i];
   }
   return NULL;
 }
 
-struct LinkedList InitLinkedList(int nodeCount) {
-  struct LinkedList list;
-  list.nodeCount = nodeCount;
-  struct Node *head = NULL;
-  struct Node *current = NULL;
+struct LinkedList *InitLinkedList(int nodeCount) {
+  struct LinkedList *list =
+      malloc(sizeof(struct LinkedList) + sizeof(struct Node) * nodeCount);
+  list->nodeCount = nodeCount;
 
-  for (int i = 0; i < nodeCount; i++) {
-    struct Node *newNode = malloc(sizeof(struct Node));
-    newNode->data = 0;
-    newNode->nextNode = NULL;
-
-    if (head == NULL) {
-      head = newNode;
-      current = head;
-    } else {
-      current->nextNode = newNode;
-      current = newNode;
-    }
-  }
-
-  list.list[0] = head;
   return list;
 }
